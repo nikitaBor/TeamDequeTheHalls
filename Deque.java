f21c5dc6b63e0c8c1635c66f3a23248ff6b973f0
whyCantIUseMyNonStuyeduAccount <nborisov@stuy.edu>
2017-04-03 08:29:29 -0400

    readded addCareful methods

--- Deque.java
+++ Deque.java
@@ -1,13 +1,15 @@
 public interface Deque<T>{
     
     public boolean addFront(T addVal);
-     public boolean addBack(T addVal);
-     //
+    public boolean addFrontCareful(T addVal);
+    public boolean addBack(T addVal);
+    public boolean addBackCareful(T addVal);
+    //
     public T removeFront();
     public T removeFrontCarefully();
     public T removeBack();
     public T removeBackCarefully();
-    
+    */
     /*accessors to obtain the front and end of deque
       if deque is empty, then peekFront() and peekBack()
       throw NullPointerExceptions whereas peekFrontCarefully()

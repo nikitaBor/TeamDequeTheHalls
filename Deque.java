afb59d5565085489abba1409fdfb5db9e526228d
joellie43 <jlum@stuy.edu>
2017-04-02 21:54:23 -0400

    implemented peek methods + size method

--- Deque.java
+++ Deque.java
@@ -1,4 +1,5 @@
 public interface Deque<T>{
+    /*
     public boolean addFront(T addVal);
     public boolean addFrontCareful(T addVal);
     public boolean addBack(T addVal);
@@ -8,11 +9,17 @@ public interface Deque<T>{
     public T removeFrontCarefully();
     public T removeBack();
     public T removeBackCarefully();
-    //
+    */
+    /*accessors to obtain the front and end of deque
+      if deque is empty, then peekFront() and peekBack()
+      throw NullPointerExceptions whereas peekFrontCarefully()
+      and peekBackCarefully() return null    
+    */
     public T peekFront();
     public T peekFrontCarefully();
     public T peekBack();
     public T peekBackCarefully();
-    //
+    
+    //returns _size of deque
     public int size();
 }
--- SidewaysLatkes.java
+++ SidewaysLatkes.java
@@ -7,15 +7,42 @@
 
 */
 public class SidewaysLatkes<T> implements Deque{
-    private DLLNode<T> _head;
+    private DLLNode<T> _front;
     private DLLNode<T> _end;
     private int _size;
     
     public SidewaysLatkes(){
-        _head = new DLLNode(null,null,null);
+        _front = new DLLNode(null,null,null);
         _end = new DLLNode(null,null,null);
-        _head.setNext(_end);
-        _end.setPrev(_head);
+        _front.setNext(_end);
+        _end.setPrev(_front);
         _size = 0;
+    }
+
+    public T peekFront(){
+	if (_size == 0){
+	    throw new NullPointerException();}
+	else { return _front.getCargo();}
+    }
+
+    public T peekFrontCarefully(){
+	if (_size == 0){
+	    return null;}
+	else { return _front.getCargo();}
+    }
+    
+    public T peekBack(){
+	if (_size == 0){
+	    throw new NullPointerException();}
+	else { return _end.getCargo();}
+    }
+
+    public T peekBackCarefully(){
+	if (_size == 0){
+	    return null;}
+	else { return _end.getCargo();}
+    }
+    public int size(){
+        return _size;
     }
 }

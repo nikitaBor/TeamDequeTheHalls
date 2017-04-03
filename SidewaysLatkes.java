adae4c35371c5fc6e686231deb4b54257991a803
Samantha Ngo <sngo1@stuy.edu>
2017-04-03 08:37:10 -0400

    Added add methods

--- SidewaysLatkes.java
+++ SidewaysLatkes.java
@@ -1,10 +1,8 @@
 //Team DequeTheHalls(Borisov N. & Lum J. & Ngo S.)
 //APCS2 pd01
 //LAB #02: All Hands on Deque! 
-//Due: 2017-04-04
-/*
-   head->a<->b<->c<->end
-*/
+//Due: 2017-04-04 
+
 public class SidewaysLatkes<T> implements Deque{
     private DLLNode<T> _front;
     private DLLNode<T> _end;
@@ -17,6 +15,45 @@ public class SidewaysLatkes<T> implements Deque{
         _end.setPrev(_front);
         _size = 0;
     }
+
+    public boolean addFront(T addVal){
+	if(_size >= 50){
+	    throw new StackOverflowError();
+	} else if(_size == 0){
+	    DLLNode newNode = new DLLNode(addVal, null, _front);
+	    _front = newNode;
+	    _end = newNode;
+	    _size += 1;
+	} else {
+	    DLLNode newNode = new DLLNode(addVal, null, _front);
+	    _front = newNode;
+	    _front.getNext().setPrev(newNode);
+	    _size += 1;
+	}
+	return true;
+    }
+
+    public boolean addFrontCareful(T addVal){
+	if(_size >= 50){
+	    return false;
+	} else {
+	    return addFront();
+	}
+    }
+	
+    public boolean addBack(T addVal){
+	if(_size == 0){
+	    addFront();
+	} else {
+	    DLLNode newNode = new DLLNode(addVal, _end, null);
+	    _end = newNode;
+	    _end.getPrev().setNext(newNode);
+	    _size += 1;
+	}
+	return true;
+    }
+
+
     public T removeFront(){
 	if(_size == 0){
 	    throw new NullPointerException();
@@ -28,6 +65,7 @@ public class SidewaysLatkes<T> implements Deque{
 	    return retVal;
 	}
     }
+
     public T removeFrontCareful(){
 	if(_size == 0){
 	    return null;
